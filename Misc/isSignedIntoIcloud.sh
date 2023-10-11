e99773294c38bc247d45f50f64b6829f634805da
Ganidran <131908453+ganidran@users.noreply.github.com>
2023-10-11 14:25:20 -0400

    Update isSignedIntoIcloud.sh
    Forked depending if macOS is < or > OS 13

--- Misc/isSignedIntoIcloud.sh
+++ Misc/isSignedIntoIcloud.sh
@@ -6,26 +6,40 @@
 
 # Determine the current user 
 currentUser=$(stat -f "%Su" /dev/console)
-# Plist pths
+# Plist paths
 appleAccountsPlist="/Library/Preferences/SystemConfiguration/com.apple.accounts.exists.plist"
 mobileMePlist="/Users/$currentUser/Library/Preferences/MobileMeAccounts.plist"
+# macOS Version
+osVersion=$(sw_vers -productversion)
 
 ###########################
-###### CHECK PLISTS #######
+####### IF OS > 13 ########
 ###########################
 
-# Check if the com.apple.accounts.exists.plist file exists
-if [ -f "$appleAccountsPlist" ]; then
-    appleAccountValue=$(/usr/libexec/PlistBuddy -c "Print :com.apple.account.AppleAccount.exists" "$appleAccountsPlist" 2>/dev/null)
-    if [ "$appleAccountValue" == "1" ]; then
-        echo "<result>Yes</result>"
+# Check if the OS version is greater than or equal to 13.0
+if [[ "$osVersion" < 13 ]]; then
+  echo "OS Version is less than 13"
+else
+    echo "OS Version is greater than 13"
+    # Check if the com.apple.accounts.exists.plist file exists
+    if [ -f "$appleAccountsPlist" ]; then
+        appleAccountValue=$(/usr/libexec/PlistBuddy -c "Print :com.apple.account.AppleAccount.exists" "$appleAccountsPlist" 2>/dev/null)
+        if [ "$appleAccountValue" == "1" ]; then
+            echo "<result>Yes</result>"
+        else
+            echo "<result>No</result>"
+        fi
     else
         echo "<result>No</result>"
+        exit 0
     fi
-    exit 0
 fi
 
-# Check if the MobileMeAccounts.plist file exists if com.apple.accounts.exists.plist doesn't exist
+###########################
+####### IF OS < 13 ########
+###########################
+
+# Check if the MobileMeAccounts.plist file exists 
 if [ -f "$mobileMePlist" ]; then
     accountDSID=$(/usr/libexec/PlistBuddy -c "Print :Accounts:0:AccountDSID" "$mobileMePlist" 2>/dev/null)
     if [ -n "$accountDSID" ]; then
@@ -34,5 +48,5 @@ if [ -f "$mobileMePlist" ]; then
         echo "<result>No</result>"
     fi
 else
-    echo "<result>No plist files found.</result>"
+    echo "<result>No</result>"
 fi
