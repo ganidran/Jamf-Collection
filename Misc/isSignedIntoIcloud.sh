4dace9287ea8fded47703aeed9ac1257c96d8737
Ganidran <131908453+ganidran@users.noreply.github.com>
2024-04-08 11:31:13 -0400

    Update isSignedIntoIcloud.sh
    added check to output the email that is logged in and removed the OS check

--- Misc/isSignedIntoIcloud.sh
+++ Misc/isSignedIntoIcloud.sh
@@ -4,49 +4,32 @@
 ##### SET VARIABLES #######
 ###########################
 
-# Determine the current user 
+# Determine the current user
 currentUser=$(stat -f "%Su" /dev/console)
-# Plist paths
+# Define paths for relevant plist files
 appleAccountsPlist="/Library/Preferences/SystemConfiguration/com.apple.accounts.exists.plist"
 mobileMePlist="/Users/$currentUser/Library/Preferences/MobileMeAccounts.plist"
-# macOS Version
-osVersion=$(sw_vers -productversion)
+# Extract Account ID from MobileMeAccounts.plist using plutil and awk
+accountId=$(plutil -convert xml1 -o - "$mobileMePlist" | awk '/<key>AccountID<\/key>/{getline; print}' | sed -n 's/.*<string>\(.*\)<\/string>.*/\1/p')
 
 ###########################
-####### IF OS > 13 ########
+### CHECK ICLOUD STATUS ###
 ###########################
 
-# Check if the OS version is greater than or equal to 13.0
-if [[ "$osVersion" < 13 ]]; then
-  echo "OS Version is less than 13"
-else
-    echo "OS Version is greater than 13"
-    # Check if the com.apple.accounts.exists.plist file exists
-    if [ -f "$appleAccountsPlist" ]; then
-        appleAccountValue=$(/usr/libexec/PlistBuddy -c "Print :com.apple.account.AppleAccount.exists" "$appleAccountsPlist" 2>/dev/null)
-        if [ "$appleAccountValue" == "1" ]; then
-            echo "<result>Yes</result>"
-        else
-            echo "<result>No</result>"
-        fi
+# Check if the com.apple.accounts.exists.plist file exists
+if [ -f "$appleAccountsPlist" ]; then
+    # Use PlistBuddy to get the value of com.apple.account.AppleAccount.exists
+    appleAccountValue=$(/usr/libexec/PlistBuddy -c "Print :com.apple.account.AppleAccount.exists" "$appleAccountsPlist" 2>/dev/null)
+    # Check if iCloud account is logged in based on the retrieved value
+    if [ "$appleAccountValue" == "1" ]; then
+        echo "<result>Yes. $accountId logged in</result>"
+        exit 0
     else
         echo "<result>No</result>"
         exit 0
     fi
-fi
-
-###########################
-####### IF OS < 13 ########
-###########################
-
-# Check if the MobileMeAccounts.plist file exists 
-if [ -f "$mobileMePlist" ]; then
-    accountDSID=$(/usr/libexec/PlistBuddy -c "Print :Accounts:0:AccountDSID" "$mobileMePlist" 2>/dev/null)
-    if [ -n "$accountDSID" ]; then
-        echo "<result>Yes</result>"
-    else
-        echo "<result>No</result>"
-    fi
 else
+    # If the com.apple.accounts.exists.plist file doesn't exist, presume iCloud is not logged in
     echo "<result>No</result>"
+    exit 0
 fi
